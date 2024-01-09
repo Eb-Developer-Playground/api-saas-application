06662b625876a76d03aba5d1fa47a96bd9d7689b
Philip Mutua <pmutua@live.com>
2024-01-09 16:55:23 +0300

    chore: rename css file to scss

--- src/app/shared/components/containers/sidebar/sidebar.component.ts
+++ src/app/shared/components/containers/sidebar/sidebar.component.ts
@@ -7,7 +7,7 @@ import { SidebarService } from './sidebar.service';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
   @ViewChild('sidenav') sidenav: MatSidenav | any;
