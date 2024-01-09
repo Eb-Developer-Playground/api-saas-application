18e62d2ab2cb5906ed51d47dddebfc473a7c4274
Philip Mutua <pmutua@live.com>
2024-01-09 16:50:01 +0300

    chore: moved auth components

--- src/app/pages/pages.routing.module.ts
+++ src/app/pages/pages.routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LandingPage } from './landing/landing.page';
-import { LoginPage } from './login/login.page';
-import { RegisterPage } from './register/register.page';
+
+import { AuthGuard } from '../shared/_helpers';
 
 const routes: Routes = [
-    { path: '', component: LandingPage },
-    { path: 'login', component: LoginPage },
-    { path: 'register', component: RegisterPage },
+    { path: '', component: LandingPage}
 ];
 
 @NgModule({
