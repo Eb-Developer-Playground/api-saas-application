6f2cfcdd0d8b6fb074aabca0b7394efaac702e25
Philip Mutua <pmutua@live.com>
2024-01-09 16:37:09 +0300

    feat(auth): Import Auth module and add Interceptor providers #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from  '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { JwtInterceptor } from './shared/_helpers/jwt.interceptor';
+import { ErrorInterceptor, fakeBackendProvider } from './shared/_helpers';
+import { AuthenticationModule } from './features/authentication/authentication.module';
+
 
 
 
@@ -18,12 +23,21 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
+    AuthenticationModule,
     PagesModule,
-    SharedModule
+    SharedModule,
+    
     
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
