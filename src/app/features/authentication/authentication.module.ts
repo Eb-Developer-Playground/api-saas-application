1cf0abc48ab0a3e139f6e94cee86ef84a267d4cd
Philip Mutua <pmutua@live.com>
2024-01-09 16:35:27 +0300

    feat(auth): Add Auth module and Import dependent modules

--- src/app/features/authentication/authentication.module.ts
+++ src/app/features/authentication/authentication.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { RegisterPage } from './register/register.page';
+import { LoginPage } from './login/login.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AuthenticationRoutingModule,
+    SharedModule
   ],
-  declarations: []
+  declarations: [
+    RegisterPage,
+    LoginPage
+  ]
 })
 export class AuthenticationModule { }
