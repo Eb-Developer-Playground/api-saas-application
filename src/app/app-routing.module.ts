19b1af4a2ba9becbd761787a1c3f0390adb422b4
Philip Mutua <pmutua@live.com>
2024-01-09 16:36:14 +0300

    feat(auth): lazy load authentication module in app.routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
+        path: 'auth',
+        loadChildren: () => import('./features/authentication/authentication.module').then(m => m.AuthenticationModule)
+    },
+    {
         path: '',
         loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
     },
+    { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
