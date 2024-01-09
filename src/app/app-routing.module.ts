5a825b92d3c7a7bed5a1d6684f29e6bdd3cffe86
Philip Mutua <pmutua@live.com>
2024-01-09 12:02:25 +0300

    chore(app-routing.module): Lazy load pages module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingPage } from './pages/landing/landing.page';
 
 const routes: Routes = [
-    { path: '', component: LandingPage }
+    {
+        path: '',
+        loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+    },
 ];
 
 @NgModule({
