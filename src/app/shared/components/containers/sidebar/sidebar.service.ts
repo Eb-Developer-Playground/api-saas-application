bb83742505d78216c9b7b6224adc4744d0e5aac3
Philip Mutua <pmutua@live.com>
2024-01-09 11:45:00 +0300

    feat(sidebar): Implemented sidebar container

---
+++ src/app/shared/components/containers/sidebar/sidebar.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+  private toggleSidebarSubject = new Subject<void>();
+  toggleSidebar$ = this.toggleSidebarSubject.asObservable();
+
+  toggleSidebar() {
+    this.toggleSidebarSubject.next();
+  }
+}
