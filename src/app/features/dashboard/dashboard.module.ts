2fc88775e3dc70d068f50aae6150b64bf219b074
Philip Mutua <pmutua@live.com>
2024-01-10 09:59:04 +0300

    :sparkles: feat(dashboard): add dashboard module ref: #5

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DashboardRoutingModule,
+    SharedModule
   ],
   declarations: []
 })
