9e8fc423b598b1e9ba4ed4632011be285abcd2c6
Philip Mutua <pmutua@live.com>
2024-01-11 16:25:30 +0300

    :alembic: Add route [WIP] test

--- src/app/features/api-catalogue/api-catalogue-routing.module.ts
+++ src/app/features/api-catalogue/api-catalogue-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ApiCatalogueListComponent } from './api-catalogue-list/api-catalogue-list.component';
+import { ApiDetailComponent } from './api-detail/api-detail.component';
 
 
 const routes: Routes = [
-    { path: '', component: ApiCatalogueListComponent }
+    { path: '', component: ApiCatalogueListComponent },
+    { path: 'detail', component: ApiDetailComponent }
 ];
 
 @NgModule({
