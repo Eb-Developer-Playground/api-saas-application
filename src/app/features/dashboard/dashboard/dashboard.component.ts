36f7aa1cfb6a72d702ef3c396a4eea5a76e8dfb2
Philip Mutua <pmutua@live.com>
2024-01-12 16:55:04 +0300

    :alembic: chore: remove code was testing integration with redoc

--- src/app/features/dashboard/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +9,6 @@ export class DashboardComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
