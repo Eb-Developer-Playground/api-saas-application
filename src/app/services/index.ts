aeb8e419f6a3ffcaae0cf127f7d5c5feff953d75
Philip Mutua <pmutua@live.com>
2024-01-16 18:16:11 +0300

    Deploy documentation to GitHub Pages

--- src/app/services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './account.service';
-export * from './ui/alert.service';
-export * from './api.service';
-export * from './ui/redoc.service';
-export * from './cache.service';
-export * from './api-key.service';
-export * from './ui/subscription.service';
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    title: 'Production Environment Heading',
-    apiUrl: 'http://localhost:4000'
-  };
