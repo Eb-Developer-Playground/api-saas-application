0d77f54618e1e988ed92ca53c2f42412ac4ae717
Philip Mutua <pmutua@live.com>
2024-01-16 17:57:51 +0300

    remove generated compodoc documenation in main branch

--- scripts/deploy-docs.bat
+++ scripts/deploy-docs.bat
@@ -3,16 +3,16 @@
 REM Step 1: Copy documentation to gh-pages folder
 xcopy /s /i /e documentation\* gh-pages\
 
-REM Step 2: Create or switch to gh-pages branch
-git checkout --orphan gh-pages
+@REM REM Step 2: Create or switch to gh-pages branch
+@REM git checkout --orphan gh-pages
 
-REM Step 3: Add files to the staging area
-git --work-tree=gh-pages add --all
+@REM REM Step 3: Add files to the staging area
+@REM git --work-tree=gh-pages add --all
 
-REM Step 4: Commit changes
-git --work-tree=gh-pages commit -m "Deploy documentation to GitHub Pages"
+@REM REM Step 4: Commit changes
+@REM git --work-tree=gh-pages commit -m "Deploy documentation to GitHub Pages"
 
-REM Step 5: Force push to gh-pages branch
-git push origin HEAD:gh-pages --force
+@REM REM Step 5: Force push to gh-pages branch
+@REM git push origin HEAD:gh-pages --force
 
-echo Documentation deployed to GitHub Pages.
+@REM echo Documentation deployed to GitHub Pages.
