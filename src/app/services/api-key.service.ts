bd2748e153315552429b675334b171f1f1428304
Philip Mutua <pmutua@live.com>
2024-01-16 11:05:30 +0300

    :recycle: refactor: add api key service methods ref #6

--- src/app/services/api-key.service.ts
+++ src/app/services/api-key.service.ts
@@ -1,11 +1,37 @@
 import { Injectable } from '@angular/core';
 
+import { apiKeyList } from '../data/api-keys-list';
+
+import { ApiKey } from '../models';
+
+
 @Injectable({
     providedIn: 'root'
 })
 export class ApiKeyService {
+
+    apiKeyList: ApiKey[] = [];
+    
     generateApiKey(): string {
-        const apiKey = Math.random().toString(36).substr(2, 10);
-        return apiKey;
+        return Math.random().toString(36).substr(2, 10);
+    }
+
+    getAllApiKeys(): ApiKey[] {
+        return this.apiKeyList;
+    }
+
+    addApiKey(apiKey: ApiKey): void {
+        this.apiKeyList.push(apiKey);
+    }
+
+    deleteApiKey(apiKeyName: string): void {
+        const indexToDelete = this.apiKeyList.findIndex(apiKey => apiKey.name === apiKeyName);
+    
+        if (indexToDelete !== -1) {
+            this.apiKeyList.splice(indexToDelete, 1);
+            console.log(`API key '${apiKeyName}' deleted successfully.`);
+        } else {
+            console.log(`API key '${apiKeyName}' not found.`);
+        }
     }
 }
