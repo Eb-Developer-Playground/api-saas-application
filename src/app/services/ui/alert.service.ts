d9813e3ffe4fdbb5a7d691f9f1a6b8d84f55d26b
Philip Mutua <pmutua@live.com>
2024-01-16 09:55:44 +0300

    :recycle: refactor: alert service to use angular material snack bar

--- src/app/services/ui/alert.service.ts
+++ src/app/services/ui/alert.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+
 import { Observable, Subject } from 'rxjs';
 import { filter } from 'rxjs/operators';
-import { Alert, AlertType } from '../models/alert';
+import { Alert, AlertType } from '../../models/alert';
 
 @Injectable({ providedIn: 'root' })
 export class AlertService {
     private subject = new Subject<Alert>();
     private defaultId = 'default-alert';
 
+    constructor(private snackBar: MatSnackBar) {}
     // enable subscribing to alerts observable
     onAlert(id = this.defaultId): Observable<Alert> {
         return this.subject.asObservable().pipe(filter(x => x && x.id === id));
@@ -34,10 +38,39 @@ export class AlertService {
     alert(alert: Alert) {
         alert.id = alert.id || this.defaultId;
         this.subject.next(alert);
+        this.showSnackBar(alert);
     }
 
     // clear alerts
     clear(id = this.defaultId) {
         this.subject.next(new Alert({ id }));
+    }
+
+    private showSnackBar(alert: Alert): void {
+        const durationInSeconds = 5;
+        const snackBarRef = this.snackBar.open(alert.message, 'Close', {
+            duration: durationInSeconds *1000,
+            verticalPosition: 'top',
+            panelClass: this.getSnackBarClass(alert.type),
+        });
+    
+        snackBarRef.onAction().subscribe(() => {
+          // Handle action click if needed
+        });
+    }
+
+    private getSnackBarClass(type: AlertType): string {
+    switch (type) {
+        case AlertType.Success:
+        return 'success-snackbar';
+        case AlertType.Error:
+        return 'error-snackbar';
+        case AlertType.Info:
+        return 'info-snackbar';
+        case AlertType.Warning:
+        return 'warning-snackbar';
+        default:
+        return '';
+    }
     }
 }
