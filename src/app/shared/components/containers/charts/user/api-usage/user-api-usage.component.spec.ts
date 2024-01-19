8f3dfe2bd4e0aba45597816a7ca6d23b3d774da4
Philip Mutua <pmutua@live.com>
2024-01-19 09:34:03 +0300

    :white_check_mark: test: add passing test for user-api-usage component

--- src/app/shared/components/containers/charts/user/api-usage/user-api-usage.component.spec.ts
+++ src/app/shared/components/containers/charts/user/api-usage/user-api-usage.component.spec.ts
@@ -1,28 +1,50 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { UserApiUsageComponent } from './user-api-usage.component';
+import { MaterialModule } from 'src/app/shared/material.module';
 
-describe('ApiUsageComponent', () => {
+describe('UserApiUsageComponent', () => {
   let component: UserApiUsageComponent;
   let fixture: ComponentFixture<UserApiUsageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserApiUsageComponent ]
-    })
-    .compileComponents();
+      declarations: [UserApiUsageComponent],
+      imports: [
+        MaterialModule
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UserApiUsageComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call createChart for each canvas when authenticated', () => {
+    const createChartSpy = spyOn(component, 'createChart');
+
+    component.isAuthenticated = true;
+    fixture.detectChanges();
+
+    expect(createChartSpy).toHaveBeenCalledTimes(4);
+    expect(createChartSpy.calls.allArgs()).toEqual([
+      [component.chart1Canvas, 'Request Count (Requests)', 'rgba(75, 192, 192, 0.2)', 'rgba(75, 192, 192, 1)', component.apiUsagePerApi.map(entry => entry.requestCount)],
+      [component.chart2Canvas, 'Traffic Volume (MB)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 99, 132, 1)', component.apiUsagePerApi.map(entry => entry.trafficVolume)],
+      [component.chart3Canvas, 'Response Time (ms)', 'rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 1)', component.apiUsagePerApi.map(entry => entry.responseTime)],
+      [component.chart4Canvas, 'Error Rates (%)', 'rgba(255, 69, 0, 0.2)', 'rgba(255, 69, 0, 1)', component.apiUsagePerApi.map(entry => entry.errorRate)],
+    ]);
+  });
+
+  it('should not call createChart when not authenticated', () => {
+    const createChartSpy = spyOn(component, 'createChart');
+
+    component.isAuthenticated = false;
+    fixture.detectChanges();
+
+    expect(createChartSpy).not.toHaveBeenCalled();
   });
 });
